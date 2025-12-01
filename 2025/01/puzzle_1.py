c6fdb7f22f8531ce40136d7d8926b5b27cea1b8f
Sachin Hampal <sach.hamp@gmail.com>
2025-12-01 22:21:43 +0000

    Fix formatting

--- 2025/01/puzzle_1.py
+++ 2025/01/puzzle_1.py
@@ -1,7 +1,7 @@
 def crack_code(
     rotations_list: list[str],
     *,
-    max_dial_number: int = 100,
+    dial_limit: int = 100,
     starting_dial_number: int = 50,
 ) -> int:
     dial_number = starting_dial_number
@@ -9,9 +9,9 @@ def crack_code(
     for rotation in rotations_list:
         direction, distance = rotation[0], int(rotation[1:])
         multiplier = 1 if direction == "R" else -1
-        dial_number = (dial_number + (distance * multiplier)) % max_dial_number
-        
-        # True condition resolve to `1` whereas false conditions resolve to `0`
+        dial_number = (dial_number + (distance * multiplier)) % dial_limit
+
+        # True conditions resolve to `1` whereas false conditions resolve to `0`
         count += dial_number == 0
 
     return count
--- 2025/01/puzzle_2.py
+++ 2025/01/puzzle_2.py
@@ -1,26 +1,25 @@
 def crack_code(
     rotations_list: list[str],
     *,
-    max_dial_number: int = 100,
+    dial_limit: int = 100,
     starting_dial_number: int = 50,
 ) -> int:
     current_dial_number = starting_dial_number
     count = 0
     for rotation in rotations_list:
         direction, distance = rotation[0], int(rotation[1:])
-        complete_rotations, remainder = divmod(distance, max_dial_number)
+        complete_rotations, remainder = divmod(distance, dial_limit)
         count += complete_rotations
         multiplier = 1 if direction == "R" else -1
-        new_dial_number = (
-            current_dial_number + (remainder * multiplier)
-        ) % max_dial_number
+        new_dial_number = (current_dial_number + (remainder * multiplier)) % dial_limit
         add_extra_rotation = (
-            (direction == "R" and new_dial_number < current_dial_number)
-            or (direction == "L" and new_dial_number > current_dial_number)
+            direction == "R" and new_dial_number < current_dial_number
+        ) or (direction == "L" and new_dial_number > current_dial_number)
+
+        # True conditions resolve to `1` whereas false conditions resolve to `0`
+        count += new_dial_number == 0 or (
+            current_dial_number != 0 and add_extra_rotation
         )
-        
-        # True condition resolve to `1` whereas false conditions resolve to `0`
-        count += new_dial_number == 0 or (current_dial_number != 0 and add_extra_rotation)
         current_dial_number = new_dial_number
 
     return count
